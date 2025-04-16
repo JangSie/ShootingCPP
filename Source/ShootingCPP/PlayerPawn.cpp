a153e5552e698c128eb7ae1f3da4675346d5cb4d
JangSie <wksqkddnf@gmail.com>
2025-04-16 17:33:06 +0900

    플레이어 박스 컴포넌트/ 매쉬 컴포넌트 추가

---
+++ Source/ShootingCPP/PlayerPawn.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawn.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Tick 함수를 사용할 때는 ture 사용 필요 (사용하겠다는 소리) p482
+
+
+	// 오브젝트의 충돌을 담당하는 컴포넌트(기능)
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	// TEXT(컴포넌트 이름)
+	SetRootComponent(BoxComp); // rootComponent 지정 
+	// #include "Components/BoxComponent.h" - 써주지 않으면 찾지 못함/ 헤더는 class로 찾았지면 여긴 include 해줘야함
+
+
+	// 컴포넌트 생성하지 않으면 공간만 생성되고 컴포넌트는 생성x
+	MeshComp = CreateDefaultSubobject <UStaticMeshComponent>(TEXT("My Static Mesh"));
+	MeshComp->SetupAttachment(BoxComp);
+	// 컴포넌트를 계층 구조로 만들어줌
+	// = 자식컴포넌트->SetupAttachment(부모될 컴포넌트);
+
+
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/ShootingCPP/PlayerPawn.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+//#include "Components/BoxComponent.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class SHOOTINGCPP_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// 인풋 관련 세팅 메서드
+
+
+public:
+	// 전방 선언
+	// 이 뒤에 타입이 클래스 타입이란 걸 알려줬기 때문에
+	// 컴파일러는 class라고 인지하고 통과시켜준다. (include가 주석이 되었는데도 오류 안뜸)
+	// 그래서 왜 전방 선언을 하느냐, 의존성이 끊어진다
+	// 의존성이 끊어진다는 건 PlayerPawn 클래스와 UBoxComponent 클래스가
+	// 관계가 없어진다라고 표현할 수 있다 = 코드가 유연해진다(모듈화도 잘 됨)
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* BoxComp;
+	// .h파일에선 웬만하면 전방 선언하는 편임 
+
+	// 메시 컴포넌트 
+	// 오브젝트에 그래픽 렌더링을 할 수 있게하는 기능 (표면 같은?
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* MeshComp;
+
+};
