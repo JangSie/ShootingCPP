ec22a5586f06d25611c5177cb99ce63f7a84f952
JangSie <wksqkddnf@gmail.com>
2025-04-16 16:17:35 +0900

    슈팅맵 카메라 및 조명 세팅

---
+++ Source/ShootingCPP/ShootingGameModeBa.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootingGameModeBa.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGCPP_API AShootingGameModeBa : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
